71faa336eab1f6f87201e287991cc20f6e1a51ed
Alvin <anpham2407@gmail.com>
2023-10-21 12:08:56 +0700

    handle order market place

--- src/models/store.ts
+++ src/models/store.ts
@@ -8,14 +8,35 @@ import { Order } from './order';
 
 @Entity()
 export class Store extends MedusaStore {
-    @OneToMany(() => User, (user) => user?.store)
-  members?: User[];
+  //   @OneToMany(() => User, (user) => user?.store)
+  // members?: User[];
 
-    @OneToMany(() => Product, (product) => product?.store)
-  products?: Product[];
+  //   @OneToMany(() => Product, (product) => product?.store)
+  // products?: Product[];
 
-    @OneToMany(() => Order, (order) => order.store)
-    @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
-    orders: Order[];
+  //   @OneToMany(() => Order, (order) => order.store)
+  //   @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+  // orders: Order[];
+
+
+  @OneToMany(() => User, (user) => user.store)
+  @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+  members: User[];
+
+  @OneToMany(() => Product, (product) => product.store)
+  @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+  products: Product[];
+
+  @OneToMany(() => Order, (order) => order.store)
+  @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+  orders: Order[];
+
+  // @OneToMany(() => Invite, (invite) => invite.store)
+  // @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+  // invites: Invite[];
+
+  // @OneToMany(() => Role, (role) => role.store)
+  // @JoinColumn({ name: 'id', referencedColumnName: 'store_id' })
+  // roles: Role[];
 
 }
